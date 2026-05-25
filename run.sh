b082bfff4e461de741430cdd0dcb1678c67d0ef8
th_rous <113802315+TRouR@users.noreply.github.com>
2026-05-25 18:40:01 +0200

    Integrate refactored Group_B branch into develop's layout

---
+++ run.sh
@@ -0,0 +1,17 @@
+# 1. Load the required software onto the compute node
+module purge
+module load nextflow
+module load singularityCE/4.3
+
+# 2. Apptainer / Singularity image cache
+export NXF_APPTAINER_CACHEDIR=/data/upfagb/jvillanueva/apptainer_cache/
+export NXF_SINGULARITY_CACHEDIR=/data/upfagb/jvillanueva/apptainer_cache/
+
+# 3. Run
+echo "=== Llançant Nextflow Test ==="
+nextflow run test_groupB.nf \
+    -profile groupB,slurm,singularity \
+    --input assets/samplesheet_smoke.csv \
+    --outdir results_test \
+    -ansi-log \
+    -resume
